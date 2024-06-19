3c48aef0edd42d9d29e32dbbee842374559e083e
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-19 13:18:13 +0200

    Conseguidas las operaciones

--- index.js
+++ index.js
@@ -4,19 +4,55 @@ const port = 3000
 
 calculadoraBackend.use(express.static('static/front'))
 
-calculadoraBackend.get("/hola", (req, res) =>{
+calculadoraBackend.get("/hola", (req, res) => {
     res.send("Hola Calculadora")
 })
 
-const suma = (req, res) => {
+const suma = ("/suma",(req, res) => {
     var sumando1 = Number(req.query.parametro1)
     var sumando2 = Number(req.query.parametro2)
-    var resultado = sumando1 + sumando2
-    
-    res.send(`SUMA: ${sumando1} + ${sumando2} = ${resultado}`)
-}
+    var sumacion = sumando1 + sumando2
 
-calculadoraBackend.get("/calculo", suma)
+    res.send(`SUMA: ${sumando1} + ${sumando2} = ${sumacion}`)
+})
+
+const resta = ("/resta",(req, res) => {
+    var restando1 = Number(req.query.parametro1)
+    var restando2 = Number(req.query.parametro2)
+    var restacion = restando1 - restando2
+
+    res.send(`RESTA: ${restando1} - ${restando2} = ${restacion}`)
+})
+
+const multi = ("/multi",(req, res) => {
+    var multi1 = Number(req.query.parametro1)
+    var multi2 = Number(req.query.parametro2)
+    var multacion = multi1 * multi2
+
+    res.send(`MULTIPLICA: ${multi1} x ${multi2} = ${multacion}`)
+})
+
+const divide = ("/divide",(req, res) => {
+    var divide1 = Number(req.query.parametro1)
+    var divide2 = Number(req.query.parametro2)
+    var division = divide1 / divide2
+    res.send(`DIVISIÓN: ${divide1} / ${divide2} = ${division}`)
+})
+
+const potencia = ("/potencia",(req, res) => {
+    var base = Number(req.query.parametro1)
+    var potenciador = Number(req.query.parametro2)
+    var resultado = Math.pow(base, potenciador)
+
+    res.send(`POTENCIA: ${base} elevado a ${potenciador} = ${resultado}`)
+})
+
+calculadoraBackend.get("/suma", suma)
+calculadoraBackend.get("/resta", resta)
+calculadoraBackend.get("/multi", multi)
+calculadoraBackend.get("/divide", divide)
+calculadoraBackend.get("/potencia", potencia)
+
 
 calculadoraBackend.listen(port, () => {
     console.log(`Calculadora escuchando en http://localhost:${port}`)
