136e42a143cffaa1ee3ed455acf06e0e2b545736
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-19 13:33:31 +0200

    Entrega a cliente

--- index.js
+++ index.js
@@ -8,43 +8,43 @@ calculadoraBackend.get("/hola", (req, res) => {
     res.send("Hola Calculadora")
 })
 
-const suma = ("/suma",(req, res) => {
+const suma = ("/suma", (req, res) => {
     var sumando1 = Number(req.query.parametro1)
     var sumando2 = Number(req.query.parametro2)
     var sumacion = sumando1 + sumando2
 
-    res.send(`SUMA: ${sumando1} + ${sumando2} = ${sumacion}`)
+    res.send(`${sumando1} + ${sumando2} = ${sumacion}`)
 })
 
-const resta = ("/resta",(req, res) => {
+const resta = ("/resta", (req, res) => {
     var restando1 = Number(req.query.parametro1)
     var restando2 = Number(req.query.parametro2)
     var restacion = restando1 - restando2
 
-    res.send(`RESTA: ${restando1} - ${restando2} = ${restacion}`)
+    res.send(`${restando1} - ${restando2} = ${restacion}`)
 })
 
-const multi = ("/multi",(req, res) => {
+const multi = ("/multi", (req, res) => {
     var multi1 = Number(req.query.parametro1)
     var multi2 = Number(req.query.parametro2)
     var multacion = multi1 * multi2
 
-    res.send(`MULTIPLICA: ${multi1} x ${multi2} = ${multacion}`)
+    res.send(`${multi1} x ${multi2} = ${multacion}`)
 })
 
-const divide = ("/divide",(req, res) => {
+const divide = ("/divide", (req, res) => {
     var divide1 = Number(req.query.parametro1)
     var divide2 = Number(req.query.parametro2)
     var division = divide1 / divide2
-    res.send(`DIVISIÓN: ${divide1} / ${divide2} = ${division}`)
+    res.send(`${divide1} / ${divide2} = ${division}`)
 })
 
-const potencia = ("/potencia",(req, res) => {
+const potencia = ("/potencia", (req, res) => {
     var base = Number(req.query.parametro1)
     var potenciador = Number(req.query.parametro2)
     var resultado = Math.pow(base, potenciador)
 
-    res.send(`POTENCIA: ${base} elevado a ${potenciador} = ${resultado}`)
+    res.send(`${base} ^ ${potenciador} = ${resultado}`)
 })
 
 calculadoraBackend.get("/suma", suma)
